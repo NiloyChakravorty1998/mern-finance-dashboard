572a636959b88991cdc2e9006818d6e34bc7ff35
Niloy Chakravorty <chakravortyniloy@gmail.com>
2023-11-12 20:58:51 +0530

    added charts

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,13 +1,111 @@
 import DashboardBox from '@/components/DashboardBox'
 import { useGetKpisQuery } from '@/store/api'
+import { useMemo } from 'react';
+import { useTheme } from "@mui/material";
+import {
+  ResponsiveContainer, AreaChart,
+  CartesianGrid, XAxis, YAxis, Tooltip,
+  Area, Line, Legend, LineChart
+} from 'recharts';
+import BoxHeader from '@/components/BoxHeader';
 
 const Row1 = () => {
-  const {data} = useGetKpisQuery();
+  //BRING IN PALLETE
+  const { palette } = useTheme();
+  const { data } = useGetKpisQuery();
+  const revenueExpenses = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue, expenses }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: revenue,
+          expenses: expenses
+        };
+      })
+    );
+  }, [data]);
+
+  const revenueProfit = useMemo(() => {
+    return (
+      data &&
+      data[0].monthlyData.map(({ month, revenue, expenses }) => {
+        return {
+          name: month.substring(0, 3),
+          revenue: revenue,
+          profit: (revenue - expenses).toFixed(2)
+        };
+      })
+    );
+  }, [data]);
+
   return (
     <>
-    <DashboardBox bgcolor="#fff" gridArea="a"></DashboardBox>
-        <DashboardBox bgcolor="#fff" gridArea="b"></DashboardBox>
-        <DashboardBox bgcolor="#fff" gridArea="c"></DashboardBox>
+      <DashboardBox gridArea="a">
+        <BoxHeader title={"Revenue and Expenses"}
+          subtitle={"top line represents revenue, bottom line represents expenses"}
+          sideText={"+4%"}></BoxHeader>
+        <ResponsiveContainer width="100%" height="100%">
+          <AreaChart
+            width={500}
+            height={400}
+            data={revenueExpenses}
+            margin={{
+              top: 15,
+              right: 25,
+              left: -10,
+              bottom: 60,
+            }}
+          >
+            <defs>
+              <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5} />
+                <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0} />
+              </linearGradient>
+              <linearGradient id="colorExpenses" x1="0" y1="0" x2="0" y2="1">
+                <stop offset="5%" stopColor={palette.primary[300]} stopOpacity={0.5} />
+                <stop offset="95%" stopColor={palette.primary[300]} stopOpacity={0} />
+              </linearGradient>
+            </defs>
+            <XAxis dataKey="name" tickLine={false} style={{ fontSize: "10px" }} />
+            <YAxis axisLine={{ strokeWidth: "0" }} domain={[8000, 23000]} tickLine={false} style={{ fontSize: "10px" }} />
+            <Tooltip />
+            <Area type="monotone" dataKey="revenue" stroke={palette.primary.main}
+              fillOpacity={1} fill="url(#colorRevenue)" dot={true} />
+            <Area type="monotone" dataKey="expenses" stroke={palette.primary.main}
+              fillOpacity={1} fill="url(#colorExpenses)" dot={true} />
+          </AreaChart>
+        </ResponsiveContainer>
+      </DashboardBox>
+
+      <DashboardBox gridArea="b">
+      <BoxHeader title={"Profit and Revenue"}
+          subtitle={"top line represents revenue, bottom line represents expenses"}
+          sideText={"+4%"}></BoxHeader>
+        <ResponsiveContainer width="100%" height="100%">
+          <LineChart
+            data={revenueProfit}
+            margin={{
+              top: 20,
+              right: 0,
+              left: -10,
+              bottom: 55,
+            }}
+          > 
+            <CartesianGrid vertical={false} stroke={palette.grey[800]}/>
+            <XAxis dataKey="name" tickLine={false} style={{ fontSize: "10px" }} />
+            <YAxis yAxisId="left" axisLine={false} 
+             tickLine={false} style={{ fontSize: "10px" }} />
+             <YAxis yAxisId="right" axisLine={false} orientation='right'
+             tickLine={false} style={{ fontSize: "10px" }} />
+            <Tooltip />
+            <Legend height={20} wrapperStyle={{margin:'0 0 10px 0'}}/>
+            <Line yAxisId='left' type='monotone' dataKey='profit' stroke={palette.tertiary[500]} />
+            <Line yAxisId='right' type='monotone' dataKey='revenue' stroke={palette.primary.main} />
+          </LineChart>
+        </ResponsiveContainer>
+      </DashboardBox>
+      <DashboardBox bgcolor="#fff" gridArea="c"></DashboardBox>
     </>
   )
 }
--- client/src/scenes/dashboard/index.tsx
+++ client/src/scenes/dashboard/index.tsx
@@ -1,5 +1,5 @@
 import DashboardBox from "@/components/DashboardBox";
-import { Box, useMediaQuery} from "@mui/material"
+import { Box, useMediaQuery } from "@mui/material"
 import Row1 from "./Row1";
 import Row2 from "./Row2";
 import Row3 from "./Row3";
@@ -19,7 +19,7 @@ const gridTemplateLargeScreens = `
     "g h j"
 `;
 
-const gridTemplateSmallScreens= `
+const gridTemplateSmallScreens = `
 "a"
 "a"
 "a"
@@ -52,22 +52,24 @@ const gridTemplateSmallScreens= `
 "j"
 `
 const Dashboard = (props: Props) => {
-    const isAboveMediumScreens= useMediaQuery("(min-width: 1200px)")
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1200px)")
   return (
-    <Box width="100%" height="100%" display="grid" gap="1.5rem"
-    sx={isAboveMediumScreens ? {
-        gridTemplateColumns:"repeat(3,minmax(370px,1fr))",
-        gridTemplateRows:"repeat(10,minmax(60px,1fr))",
-        gridTemplateAreas: gridTemplateLargeScreens} :
-        {
-            gridAutoColumns:"1fr",
-            gridAutoRows:"80px",
-            gridTemplateAreas: gridTemplateSmallScreens}
+      <Box width="100%" height="100%" display="grid" gap="1.5rem"
+        sx={isAboveMediumScreens ? {
+          gridTemplateColumns: "repeat(3,minmax(370px,1fr))",
+          gridTemplateRows: "repeat(10,minmax(60px,1fr))",
+          gridTemplateAreas: gridTemplateLargeScreens
+        } :
+          {
+            gridAutoColumns: "1fr",
+            gridAutoRows: "80px",
+            gridTemplateAreas: gridTemplateSmallScreens
+          }
         }>
-       <Row1/>
-       <Row2/>
-       <Row3/>
-        </Box>
+        <Row1 />
+        <Row2 />
+        <Row3 />
+      </Box>
   )
 }
 
